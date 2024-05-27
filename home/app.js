6c8c7a77e03cf2346df1e0b446066282b55dc5ca
Emanuelprime <emanuelll3000@gmail.com>
2024-05-27 20:26:55 -0300

    att

---
+++ home/app.js
@@ -0,0 +1,19 @@
+if (localStorage.getItem('theme')== 'light'){
+    setlightMode();
+    if(document.getElementById('checkbox').checked){
+        localStorage.setItem('checkbox', true)
+    }
+}
+
+    function setlightMode(){
+        let isDark = document.body.toggle('lightmode');
+
+        if (isDark){
+            setlightMode.checked = true;
+            localStorage.setItem('theme', 'light');
+            document.getElementById('checkbox').setAttribute('checkbox', 'checked');
+        }else{
+            setlightMode.checked = true;
+            localStorage.removeItem('theme', 'light');
+        }
+    }
