a7474e39c104f7c062db79fcd003a98a3132561b
Emanuelprime <emanuelll3000@gmail.com>
2024-06-03 20:18:08 -0300

    att

--- home/app.js
+++ home/app.js
@@ -1,8 +1,8 @@
 document.getElementById('toggle-button').addEventListener('click', function() {
     var stylesheet = document.getElementById('stylesheet');
-    if (stylesheet.getAttribute('href') === 'style1.css') {
-        stylesheet.setAttribute('href', 'style2.css');
+    if (stylesheet.getAttribute('href') === 'styleBlack.css') {
+        stylesheet.setAttribute('href', 'styleWhite.css');
     } else {
-        stylesheet.setAttribute('href', 'style1.css');
+        stylesheet.setAttribute('href', 'styleBlack.css');
     }
 });
