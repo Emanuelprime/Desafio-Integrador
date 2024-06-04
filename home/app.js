8b01c0f25dc0b911bd116bafd9c7f88b7a80ca32
Emanuelprime <emanuelll3000@gmail.com>
2024-06-04 19:40:37 -0300

    att

--- home/app.js
+++ home/app.js
@@ -6,3 +6,22 @@ document.getElementById('toggle-button').addEventListener('click', function() {
         stylesheet.setAttribute('href', 'styleBlack.css');
     }
 });
+
+const botaoAumentarFonte = document.getElementById('botao-aumentar-fonte');
+const botaoDiminuirFonte = document.getElementById('botao-diminuir-fonte');
+
+botaoAumentarFonte.addEventListener('click', () => {
+  const fontSize = getComputedStyle(document.body).fontSize;
+  const newSize = parseInt(fontSize) + 2; // Aumentar em 2px
+
+  document.body.style.fontSize = `${newSize}px`;
+});
+
+botaoDiminuirFonte.addEventListener('click', () => {
+  const fontSize = getComputedStyle(document.body).fontSize;
+  const newSize = parseInt(fontSize) - 2; // Diminuir em 2px
+
+  if (newSize >= 12) { // Limite mínimo de tamanho de fonte
+    document.body.style.fontSize = `${newSize}px`;
+  }
+});
